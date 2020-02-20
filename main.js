8d922dc76094641fb1e03fe8e44c395d6f2fecd4
Lebogang Olifant <lebogangodeveloper@gmail.com>
2020-02-20 15:41:17 +0200

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+// mobile nav
+function myFunction() {
+    const x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
