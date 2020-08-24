2af1461be54bea7d9b7f8eac9dd76ad3c51da6a0
lebogang B Olifant <lebogangodeveloper@gmail.com>
2020-08-24 14:07:49 +0200

    website redesign

--- main.js
+++
@@ -1,9 +0,0 @@
-// mobile nav
-function myFunction() {
-    const x = document.getElementById("navLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
