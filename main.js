876ae837f93213da6b7e225a86d1391037e93047
lebogang B Olifant <lebogangodeveloper@gmail.com>
2020-08-25 15:34:28 +0200

    format docs

---
+++ main.js
@@ -0,0 +1,9 @@
+// menu toggle
+function toggleMenu() {
+  const menuToggle = document.querySelector('.toggle');
+  const navigation = document.querySelector('.nav');
+  const menuToggleClose = document.querySelector('.toggleClose');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+  menuToggleClose.classList.toggle('active');
+}
